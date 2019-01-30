237030ff9a993ccee23530e5b8e99306c15f6ccf
mariansam <margotka000@gmail.com>
2019-01-30 17:39:37 +0100

    Add Python3

---
+++ mktoken3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import hashlib
+import base64
+import datetime
+
+import urllib.request
+import xml.etree.ElementTree as ET
+import ssl
+
+import sys
+
+ctx = ssl.create_default_context();
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+res = urllib.request.urlopen('https://' + sys.argv[1] + '/login.aspx?gethx=' + sys.argv[2], context=ctx).read()
+xml = ET.fromstring(res)
+
+pwd = sys.argv[3]
+ikod = xml[2].text
+salt = xml[3].text
+typ = xml[1].text
+name = sys.argv[2]
+
+hashpass = base64.b64encode(hashlib.sha512((salt+ikod+typ+pwd).encode('utf-8')).digest())
+
+now = datetime.datetime.today().strftime('%Y%m%d')
+
+rawtoken = '*login*' + name + '*pwd*' + hashpass.decode('utf-8') + '*sgn*ANDR' + now
+
+token = base64.b64encode(hashlib.sha512(rawtoken.encode('utf-8')).digest()).decode('utf-8')
+token = token.replace('\\', '_')
+token = token.replace('/', '_')
+token = token.replace('+', '-')
+
+print(token)
+
