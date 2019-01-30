32d2d6f80cbe5585b0d0baede842c806d12383aa
mariansam <margotka000@gmail.com>
2019-01-30 18:02:57 +0100

    Add library

--- mktoken.py
+++ mktoken.py
@@ -8,30 +8,30 @@ import urllib2
 import xml.etree.ElementTree as ET
 import ssl
 
-import sys
+def generate_token(domain, user, password):
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    res = urllib2.urlopen('https://' + domain + '/login.aspx?gethx=' + user, context=ctx).read()
+    xml = ET.fromstring(res)
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-res = urllib2.urlopen('https://' + sys.argv[1] + '/login.aspx?gethx=' + sys.argv[2], context=ctx).read()
-xml = ET.fromstring(res)
+    ikod = xml[2].text
+    salt = xml[3].text
+    typ = xml[1].text
 
-pwd = sys.argv[3]
-ikod = xml[2].text
-salt = xml[3].text
-typ = xml[1].text
-name = sys.argv[2]
+    hashpass = base64.b64encode(hashlib.sha512(salt+ikod+typ+password).digest())
 
-hashpass = base64.b64encode(hashlib.sha512(salt+ikod+typ+pwd).digest())
+    now = datetime.datetime.today().strftime('%Y%m%d')
 
-now = datetime.datetime.today().strftime('%Y%m%d')
+    rawtoken = '*login*' + user + '*pwd*' + hashpass + '*sgn*ANDR' + now
 
-rawtoken = '*login*' + name + '*pwd*' + hashpass + '*sgn*ANDR' + now
+    token = base64.b64encode(hashlib.sha512(rawtoken).digest())
+    token = token.replace('\\', '_')
+    token = token.replace('/', '_')
+    token = token.replace('+', '-')
 
-token = base64.b64encode(hashlib.sha512(rawtoken).digest())
-token = token.replace('\\', '_')
-token = token.replace('/', '_')
-token = token.replace('+', '-')
+    return token
 
-print(token)
-
+if __name__ == "__main__":
+    import sys
+    print generate_token(sys.argv[1], sys.argv[2], sys.argv[3])
--- mktoken3.py
+++ mktoken3.py
@@ -8,31 +8,30 @@ import urllib.request
 import xml.etree.ElementTree as ET
 import ssl
 
-import sys
+def generate_token(domain, user, password):
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    res = urllib.request.urlopen('https://' + domain + '/login.aspx?gethx=' + user, context=ctx).read()
+    xml = ET.fromstring(res)
 
+    ikod = xml[2].text
+    salt = xml[3].text
+    typ = xml[1].text
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-res = urllib.request.urlopen('https://' + sys.argv[1] + '/login.aspx?gethx=' + sys.argv[2], context=ctx).read()
-xml = ET.fromstring(res)
+    hashpass = base64.b64encode(hashlib.sha512((salt+ikod+typ+password).encode('utf-8')).digest())
 
-pwd = sys.argv[3]
-ikod = xml[2].text
-salt = xml[3].text
-typ = xml[1].text
-name = sys.argv[2]
+    now = datetime.datetime.today().strftime('%Y%m%d')
 
-hashpass = base64.b64encode(hashlib.sha512((salt+ikod+typ+pwd).encode('utf-8')).digest())
+    rawtoken = '*login*' + user + '*pwd*' + hashpass.decode('utf-8') + '*sgn*ANDR' + now
 
-now = datetime.datetime.today().strftime('%Y%m%d')
+    token = base64.b64encode(hashlib.sha512(rawtoken.encode('utf-8')).digest()).decode('utf-8')
+    token = token.replace('\\', '_')
+    token = token.replace('/', '_')
+    token = token.replace('+', '-')
 
-rawtoken = '*login*' + name + '*pwd*' + hashpass.decode('utf-8') + '*sgn*ANDR' + now
+    return token
 
-token = base64.b64encode(hashlib.sha512(rawtoken.encode('utf-8')).digest()).decode('utf-8')
-token = token.replace('\\', '_')
-token = token.replace('/', '_')
-token = token.replace('+', '-')
-
-print(token)
-
+if __name__ == "__main__":
+    import sys
+    print(generate_token(sys.argv[1], sys.argv[2], sys.argv[3]))
